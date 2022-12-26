d164bb0784ed21c32776d58ba428793a4a5532bd
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-26 11:11:23 +0500

    just

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (Character.isAlphabetic(character)){
+            char lowerChar = Character.toLowerCase(character);
+            if (lowerChar=='a'||lowerChar=='e'||lowerChar=='i'||lowerChar=='o'||lowerChar=='u'||lowerChar=='y')
+                System.out.println("Vowel");
+            else System.out.println("Consonant");
+        }else System.out.println("wrong alphabet!");
     }
 }
